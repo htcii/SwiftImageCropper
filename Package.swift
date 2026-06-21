6f5b3f5f1cb5a852acd976fe4051b7ae45a94ac0
htci <act@htrii.com>
2026-06-21 10:46:18 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ImageCropper",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ImageCropper",
+            targets: ["ImageCropper"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ImageCropper",
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+        .testTarget(
+            name: "ImageCropperTests",
+            dependencies: ["ImageCropper"],
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
